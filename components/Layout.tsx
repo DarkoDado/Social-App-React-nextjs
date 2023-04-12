907dc32a16358c95a2f8937e91b5d3afd36424f5
Darko Pantic <gusar97@gmail.com>
2023-04-13 00:48:21 +0200

    Work on responsive, navigation and home page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,14 +3,20 @@ import { NavigationCard } from './NavigationCard'
 import { ChildrenType } from '@/types-interfaces/ChildrenType'
 
 export const Layout = ({ children, hideNavigation }: ChildrenType) => {
+  let rightColumnClasses = ''
+  if (hideNavigation) {
+    rightColumnClasses += 'w-full'
+  } else {
+    rightColumnClasses += 'mx-3 md:mx-0 md:w-9/12'
+  }
   return (
-    <div className="flex mt-4 max-w-4xl mx-auto gap-6">
+    <div className="md:flex mt-4 max-w-4xl mx-auto gap-6">
       {!hideNavigation && (
-        <div className="w-3/12">
+        <div className="md:static w-full bottom-0 fixed md:w-3/12 -mb-5">
           <NavigationCard />
         </div>
       )}
-      <div className={hideNavigation ? 'w-full' : 'w-9/12'}>{children}</div>
+      <div className={rightColumnClasses}>{children}</div>
     </div>
   )
 }
--- components/NavigationCard.tsx
+++ components/NavigationCard.tsx
@@ -6,13 +6,15 @@ export const NavigationCard = () => {
   const router = useRouter()
   const { asPath: pathname } = router
   const activeElementClasses =
-    'flex gap-3 py-3 my-1 bg-socialBlue text-white -mx-10 px-10 rounded-md shadow-md shadow-gray-300'
+    'text-sm md:text-md flex md:gap-1 md:gap-3 py-3 my-1 bg-socialBlue text-white md:-mx-7 px-5 md:px-7 rounded-md shadow-md shadow-gray-300'
   const noneActiveElementClasses =
-    'flex gap-3 py-2 my-2 hover:bg-blue-500 hover:bg-opacity-20 -mx-4 px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300'
+    'flex text-sm md:text-md gap-1 md:gap-3 py-2 my-2 hover:bg-blue-500 hover:bg-opacity-20 md:-mx-4 px-5 md:px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300'
   return (
-    <Card>
-      <div className="px-4 py-2">
-        <h2 className="text-gray-300 font-bold mb-3">Navigation</h2>
+    <Card noPadding={true}>
+      <div className="px-4 py-2 flex md:block justify-between shadow-md shadow-gray-400 md:shadow-none">
+        <h2 className="text-gray-300 font-bold mb-3 hidden md:block">
+          Navigation
+        </h2>
 
         <Link
           href="/"
@@ -34,7 +36,7 @@ export const NavigationCard = () => {
               d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
             />
           </svg>
-          Home
+          <span className="hidden md:block">Home</span>
         </Link>
         <Link
           href="/profile/friends"
@@ -58,7 +60,7 @@ export const NavigationCard = () => {
               d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
             />
           </svg>
-          Friends
+          <span className="hidden md:block">Friends</span>
         </Link>
         <Link
           href="/saved"
@@ -82,7 +84,7 @@ export const NavigationCard = () => {
               d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
             />
           </svg>
-          Saved posts
+          <span className="hidden md:block">Saved posts</span>
         </Link>
         <Link
           href="/notifications"
@@ -106,7 +108,7 @@ export const NavigationCard = () => {
               d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
             />
           </svg>
-          Notifications
+          <span className="hidden md:block">Notifications</span>
         </Link>
         <Link href="" className={noneActiveElementClasses}>
           <svg
@@ -123,7 +125,7 @@ export const NavigationCard = () => {
               d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
             />
           </svg>
-          Logout
+          <span className="hidden md:block">Logout</span>
         </Link>
       </div>
     </Card>
